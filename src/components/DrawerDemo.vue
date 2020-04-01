4e72949a48f98ebc208d130dd4e883e773f8431c
Crack-Maker <2621161488@qq.com>
2020-04-01 23:17:13 +0800

    :twisted_rightwards_arrows: 手动merge wpy的DrawerDemo

--- src/components/DrawerDemo.vue
+++ src/components/DrawerDemo.vue
@@ -2,19 +2,51 @@
   <div class="drawer">
     <drawer
       width="200px;"
-      :show.sync="isshow"
+      :show.sync="isShow"
       show-mode="push"
       placement="left"
-      :drawer-style="{'background-color':'#35495e', width: '200px',height:'100px'}">
+      :drawer-style="{'background-color':'#35495e', width: '7rem',height:'25rem'}">
       <!-- drawer content -->
       <div slot="drawer">
-        1111111
+          <group title="侧边菜单demo" style="margin-top:20px;">
+          <cell title="Buy me a coffee" link="/user" @click.native="isShow = false"></cell>
+          <cell
+            title="Github"
+            link="http://github.com/airyland/vux"
+            value="Star me"
+            @click.native="isShow = false"
+          ></cell>
+          <cell title="Demo" link="/user" value="用户2" @click.native="isShow = false"></cell>
+          <cell title="Buy me a coffee" link="/user" @click.native="isShow = false"></cell>
+          <cell
+            title="Github"
+            link="http://github.com/airyland/vux"
+            value="Star me"
+            @click.native="isShow = false"
+          ></cell>
+        </group>
       </div>
       <div slot="drawer">
-        222222
+        <group title="信息" style="margin-top:20px;">
+          <cell title="Buy me a coffee" link="/user" @click.native="isShow = false"></cell>
+          <cell
+            title="Github"
+            link="http://github.com/airyland/vux"
+            value="Star me"
+            @click.native="isShow = false"
+          ></cell>
+          <cell title="Demo" link="/user" value="用户2" @click.native="isShow = false"></cell>
+          <cell title="Buy me a coffee" link="/user" @click.native="isShow = false"></cell>
+          <cell
+            title="Github"
+            link="http://github.com/airyland/vux"
+            value="Star me"
+            @click.native="isShow = false"
+          ></cell>
+        </group>
       </div>
       <div class="btn2">
-        <x-button @click.native="isshow=!isshow" type="primary" plain>{{this.isshow?'-':'+'}}</x-button>
+        <x-button @click.native="isShow=!isShow" type="primary" plain>{{this.isShow?'-':'+'}}</x-button>
       </div>
       <div>
         <router-view>22222</router-view>
@@ -24,21 +56,20 @@
 </template>
 
 <script>
-  import { XButton,Drawer } from 'vux'
+  import { XButton,Drawer, Group, Cell } from 'vux'
   export default {
     name: "drawerDemo",
     components: {
-       XButton,Drawer
+       XButton,Drawer,Group,Cell
     },
     data () {
       return {
-        isshow:false,
+        isShow:false,
       }
     }
   }
 </script>
 <style lang="less">
-  * { touch-action: none;touch-action: pan-y;touch-action: pan-x; }
   .btn2{
     width: 60px;
     height: 60px;
