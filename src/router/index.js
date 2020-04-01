1cef50a89da679f0db8628aff1e6414cecdd3eda
Crack-Maker <2621161488@qq.com>
2020-04-01 23:24:34 +0800

    :twisted_rightwards_arrows: 手动merge vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import LogIn from '@/views/LogIn.vue'
 import Home from '@/views/Home.vue'
+import Regist from '@/views/Regist.vue'
+import User from '@/views/User.vue'
 import TabBarDemo from '@/components/TabBarDemo.vue'
 import DrawerDemo from '@/components/DrawerDemo.vue'
+import IsLand1 from '@/views/island/IsLand1.vue'
+import IsLand2 from '@/views/island/IsLand2.vue'
+import IsLand3 from '@/views/island/IsLand3.vue'
 
 Vue.use(Router)
 
@@ -35,6 +40,31 @@ export default new Router({
       path: '/drawer',
       name: 'drawerDemo',
       component: DrawerDemo
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: User
+    },
+    {
+      path:'/regist',
+      name:'regist',
+      component:Regist
+    },
+    {
+      path: '/island1',
+      name: 'isLand1',
+      component: IsLand1
+    },
+    {
+      path: '/island2',
+      name: 'isLand2',
+      component: IsLand2
+    },
+    {
+      path: '/island3',
+      name: 'isLand3',
+      component: IsLand3
     }
   ]
 })
