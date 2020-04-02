66faad4a37c2b0a53ab06451cf46145477ad7a25
Crack-Maker <2621161488@qq.com>
2020-04-02 12:05:58 +0800

    :twisted_rightwards_arrows: 手动merge三座岛

---
+++ src/views/island/IsLand1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+	  <x-header style="width:100%;position:absolute;left:0;top:0;z-index:100;" :right-options="{showMore: true}"
+	   @on-click-more="showMenus = true">故事之岛</x-header>
+	  <h1>这里是第一座岛</h1>
+  </div>
+</template>
+
+<script>
+import { XHeader } from 'vux'
+export default {
+  name: "Home",
+  components: {
+	  XHeader
+  }
+}
+</script>
+
+<style>
+</style>
