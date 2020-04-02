0e1286dbe7015cc11cb3157746dea748195b37b9
Crack-Maker <2621161488@qq.com>
2020-04-02 12:17:00 +0800

    :twisted_rightwards_arrows: 手动merge App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,63 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div id="app" style="height:100%;">
+<!-- 	 <DrawerDemo></DrawerDemo> -->
+	 <div>
+		 <router-view></router-view>
+		 <TabBarDemo></TabBarDemo>
+	 </div>
   </div>
 </template>
 
 <script>
+import { Radio, Group, Cell, Badge, Drawer, Actionsheet, 
+		 ButtonTab, ButtonTabItem, ViewBox, XHeader, Tabbar, 
+		 TabbarItem, Loading, TransferDom} from 'vux'
+import TabBarDemo from './components/TabBarDemo.vue'
+import DrawerDemo from './components/DrawerDemo.vue'
 export default {
-  name: 'app'
+  components: { 
+	  TabBarDemo,
+	  DrawerDemo,
+	  Radio,
+    Group,
+    Cell,
+    Badge,
+    Drawer,
+    ButtonTab,
+    ButtonTabItem,
+    ViewBox,
+    XHeader,
+    Tabbar,
+    TabbarItem,
+    Loading,
+    Actionsheet
+	},
+  data () {
+      return {
+        msg: 'Hello Vue!'
+      }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'PingFangSC', arial, sans-serif;
-}
-
-* {
-  padding: 0;
-  margin: 0;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
+
+<!-- 使用vux -->
 <style lang="less">
   @import '~vux/src/styles/reset.less';
 </style>
+
+<style scoped>
+</style>
+
+
+
