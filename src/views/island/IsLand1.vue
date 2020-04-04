20f07650b8f6f8e4829b20e60a77736cdafa6138
龙腾寰宇 <2227933115@qq.com>
2020-04-04 21:33:47 +0800

    在故事之岛界面增加了文章区域以及提交成功弹窗

--- src/views/island/IsLand1.vue
+++ src/views/island/IsLand1.vue
@@ -2,16 +2,47 @@
   <div>
 	  <x-header style="width:100%;position:absolute;left:0;top:0;z-index:100;" :right-options="{showMore: true}"
 	   @on-click-more="showMenus = true">故事之岛</x-header>
-	  <h1>这里是第一座岛</h1>
+	<group :title="'My story'">
+      <x-input :placeholder="'标题'"></x-input>
+      <x-textarea :max="200" name="description" :placeholder="'内容'"></x-textarea>
+    </group>
+	<x-button mini plain @click.native="showPlugin">提交</x-button>
   </div>
 </template>
 
 <script>
-import { XHeader } from 'vux'
+import { XTextarea, Group, XInput, XButton } from 'vux'
+import { AlertModule, Alert, XSwitch, Cell, TransferDomDirective as TransferDom } from 'vux'
 export default {
   name: "Home",
+  directives: {
+    TransferDom
+  },
   components: {
-	  XHeader
+	XTextarea,
+    Group,
+    XInput,
+	XButton,
+	Cell,
+	Alert,
+    XSwitch
+  },
+  methods: {
+    onEvent (event) {
+      console.log('on', event)
+    },
+	showPlugin () {
+      AlertModule.show({
+        title: '提交成功!',
+        content: ('Submit successfully!'),
+        onShow () {
+          console.log('Module: I\'m showing')
+        },
+        onHide () {
+          console.log('Module: I\'m hiding now')
+        }
+      })
+    }
   }
 }
 </script>
