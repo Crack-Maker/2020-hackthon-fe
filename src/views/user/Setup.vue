0bbf505afedaeb84d77f7e42233394861cd43ef8
龙腾寰宇 <2227933115@qq.com>
2020-04-07 13:16:34 +0800

    增加了修改昵称界面和账号设置界面

--- src/views/user/Setup.vue
+++ src/views/user/Setup.vue
@@ -6,11 +6,11 @@
 		  <br />
 		  <div>
 			<img style="height: 3.3rem;margin-left: 1rem;
-			background-size: auto 100%;" align="left" src="../../../static/img/头像.svg"/>
+			background-size: auto 100%;border-radius: 2rem;" align="left" src="../../../static/img/aiji.jpg"/>
 		  </div>
 		  <!-- 显示用户昵称 -->
-		  <div class="name-font-style">Refuel Island
-			<img src="../../../static/img/home1.svg" />
+		  <div class="name-font-style"><p>Refuel Island</p>
+		  <div @click="linkToChangeName()" style="position: relative;right: -2.5rem;top: -1rem;"><img src="../../../static/img/home1.svg" /></div>
 		  </div>
 		  <div class="normal-font-size">Refuel Island的个性签名</div>
 		  <div style="margin-left: -10rem;position: relative;left: -5.2rem;">
@@ -19,14 +19,14 @@
 		</div>
 		<div class="normal-font-size">
 			<group>
-				<cell title="账号登陆" @click.native="handleLogin"></cell>
+				<cell title="账号登陆" @click.native="linkToLogin"></cell>
 				<br />
-				<cell title="账号设置" @click.native="handleLogin"></cell>
+				<cell title="账号设置" @click.native="linkToChangePassword"></cell>
 				<br />
-				<cell title="问题与反馈" @click.native="handleLogin"></cell>
+				<cell title="问题与反馈" @click.native="linkToLogin"></cell>
 				<br /><br /><br />
 				<box class="exit-box">
-					<x-button @click.native="handleLogin" class="exit-btn">退出</x-button>
+					<x-button @click.native="linkToUser" class="exit-btn">退出</x-button>
 				</box>
 			</group>
 		</div>
@@ -52,10 +52,25 @@ export default {
   },
   methods:{
   	//页面跳转方法
-  	handleLogin(){
+  	linkToLogin(){
   		this.$router.push({
   			path:'/Login'
   		})
+	},
+	linkToChangeName(){
+		this.$router.push({
+			path:'/ChangeName'
+		})
+	},
+	linkToChangePassword(){
+		this.$router.push({
+			path:'/ChangePassword'
+		})
+	},
+	linkToUser(){
+		this.$router.push({
+			path:'/User'
+		})
 	}
   }
 }
@@ -78,7 +93,7 @@ export default {
 }
 .exit-btn {
   border-radius: 0;
-  background-color: #667d3e;
+  background-color: #667d3e !important;
   color: #008000;
   height: 1.5rem;
   line-height: 0;
