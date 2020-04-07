a775d73c6e6b629b3880c418aafbe07b6a32ad32
龙腾寰宇 <2227933115@qq.com>
2020-04-07 14:13:23 +0800

    完善了账号设置界面

--- src/views/user/setup/ChangePassword.vue
+++ src/views/user/setup/ChangePassword.vue
@@ -1,20 +1,14 @@
 <template>
-  <div class="login">
-    <x-header
-      style="width:100%;position:absolute;left:0;top:0;z-index:100;"
-      :right-options="{showMore: true}"
-      @on-click-more="showMenus = true"
-    >账号登陆</x-header>
-    <div class="title-container">
-      <div class="title-1">Welcome to the</div>
-      <div class="title-2">ISLAND</div>
-    </div>
-    <div class="login-box">
+  <div class="changePassword">
+		<x-header style="width:100%;position:absolute;left:0;top:0;z-index:100;" :right-options="{showMore: true}"
+		 @on-click-more="showMenus = true">账号设置</x-header>
+	<div class="title-2">账号设置</div>
+	<div class="login-box">
       <group class="no-border">
         <x-input
           title="头像 |"
           name="phone"
-          placeholder="请输入手机号码"
+          placeholder="请输入新的手机号码"
           class="phone border"
           v-model="phone"
           keyboard="number"
@@ -24,11 +18,23 @@
           <img slot="label" src="../../../assets/imgs/me.svg" class="logo" />
         </x-input>
       </group>
+	  <group class="no-border">
+		<x-input title type="text" class="confirm border" placeholder="请输入验证码" v-model="code" required>
+        <x-button
+          slot="right"
+          type="primary"
+          mini
+          :disabled="getCode.disabled"
+          @click.native="handleGetCode"
+          style="width: 100px;"
+        >{{getCode.txt}}</x-button>
+		</x-input>
+	  </group>
       <group class="no-border">
         <x-input
           title="锁头 |"
           type="text"
-          placeholder="请输入密码"
+          placeholder="请输入新密码"
           class="passwd border"
           v-model="password"
           :min="6"
@@ -40,111 +46,113 @@
           <img slot="label" src="../../../assets/imgs/lock.svg" class="logo" />
         </x-input>
       </group>
-    </div>
-    <div class="forget-pass">忘记密码?</div>
+	  <group class="no-border">
+      <x-input
+        title
+        type="password"
+        placeholder="再次确认新密码"
+		class="passwdC border"
+        v-model="passwordC"
+        :min="6"
+        :max="16"
+        required
+      >
+	  <img slot="label" src="../../../assets/imgs/lock.svg" class="logo" />
+	  </x-input>
+    </group>
+	</div>
     <box class="login-btn-box">
-      <x-button @click.native="handleLogin" class="login-btn">登陆</x-button>
+      <x-button @click.native="handleConfirm" class="login-btn">登陆</x-button>
     </box>
-    <div @click="handleRegist()" class="regist">还没有账号？快去注册</div>
   </div>
 </template>
 
 <script>
 import { XInput, Box, Group, XButton } from "vux";
+
 export default {
-  name: "login",
+  name: "regist",
   data() {
     return {
-      phone: "", //用户手机号
-      password: "" //用户密码
+      phone: "", //手机号
+      code: "", //验证码
+      password: "", //密码
+      passwordC: "", //确认密码
+      getCode: {
+        txt: "获取验证码",
+        disabled: false
+      }
     };
+  },
+  methods: {
+    handleGetCode() {
+      if (this.phone) {
+        this.getCode.disabled = true;
+        this.getCode.txt = "获取中...";
+        let seconds = 60;
+        let self = this;
+        let time = setInterval(function() {
+          --seconds;
+          self.getCode.txt = seconds + "s";
+          if (seconds <= 0) {
+            clearInterval(time);
+            self.getCode.disabled = false;
+            self.getCode.txt = "获取验证码";
+          }
+        }, 1000);
+      } else {
+        this.$vux.toast.text("请先填写手机号");
+      }
+    },
+    handleConfirm() {
+      if (!this.phone || !this.code || !this.password || !this.passwordC) {
+        this.$vux.toast.text("您有未填项，不能注册");
+      } else if (this.password != this.passwordC) {
+        this.$vux.toast.text("两次输入的密码不一致");
+      } else {
+        this.$vux.toast.text("修改成功");
+      }
+    }
+  },
+  created() {
+    window.document.title = "注册";
   },
   components: {
     XInput,
     XButton,
     Group,
     Box
-  },
-  methods: {
-    change(val) {
-      console.log("on change", val);
-    },
-    //暂时设置回到user.vue界面
-    handleLogin() {
-      this.$router.push({
-        path: "/User"
-      });
-    },
-    handleRegist() {
-      this.$router.push({
-        path: "/Regist"
-      });
-    }
   }
 };
 </script>
 
 <style scoped lang="less">
-.input-box {
-  background-color: #ffffff;
-}
-
-.login {
+.changePassword {
   background-image: url(../../../assets/imgs/bg-island.png);
   background-repeat: no-repeat;
   background-position: 4.5rem 0rem;
   background-size: 8rem;
   height: 19rem;
 }
-
-.forget-pass {
-  font-size: 0.45rem;
-  text-align: center;
+.normal-font-size{
+  font-size: 0.48rem;
 }
-
-.title-container {
-  padding-top: 1rem;
-  margin-left: 0.7rem;
-}
-
-.title-1 {
-  letter-spacing: 0.05rem;
-  font-size: 0.51rem;
-  font-family: "Noto Sans CJK SC", "Source Han Sans CN";
-  font-weight: 400;
-  text-align: left;
-  color: #93b04a;
-}
-
 .title-2 {
-  letter-spacing: 0.15rem;
-  font-size: 1.2rem;
+  letter-spacing: 0.1rem;
+  margin-left: 0.6rem;
+  font-size: 0.75rem;
   font-family: "Noto Sans CJK SC", "Source Han Sans CN";
-  font-weight: 500;
+  font-weight: 1000;
   text-align: left;
   color: #667d3e;
 }
-
-.login-box {
-  width: 10.1rem;
-  margin-top: 3.5rem;
-  margin-left: 0.4rem;
-}
-
-.forget-pass {
-  text-align: right;
-  font-size: 0.39rem;
-  margin: 0.55rem;
-  color: #707070;
-}
-
 .border {
   border: 1px solid #fdfdfd;
-  border-radius: 45px;
+  border-radius: 10px;
   -webkit-box-shadow: 1px 4px 5px #c3c3c3;
   box-shadow: 1px 4px 5px #c3c3c3;
   margin: 0.2rem;
-  padding: 0.4rem 0.8rem;
+  padding: 0.2rem 0.6rem;
   text-indent: 0.5em;
   font-size: 0.45rem;
 }
@@ -155,11 +163,17 @@ export default {
   width: 0.65rem;
   border-right: 0.02rem solid #afb3b5;
 }
+.login-box {
+  margin-top: 2rem;
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
 .login-btn-box {
   margin: 0 1rem;
+  margin-top: 4rem;
 }
 .login-btn {
-  border-radius: 0;
+  border-radius: 10px;
   background-color: #667d3e;
   color: white;
   height: 1.5rem;
@@ -169,37 +183,7 @@ export default {
   font-family: zzgf, Arial;
   font-weight: 400;
 }
-.regist {
-  font-size: 0.35rem;
-  color: #5cc2b6;
-  margin-top: 0.35rem;
-}
 </style>
+
 <style>
-.no-border div:before {
-  display: none;
-}
-.no-border div:after {
-  display: none;
-}
-.no-border div {
-  margin-top: 0.1rem;
-}
-input::-webkit-input-placeholder {
-  color: #afb3b5;
-  letter-spacing: 0.1rem;
-}
-
-input::placeholder {
-  color: #afb3b5;
-  letter-spacing: 0.02rem;
-}
-
-.vux-input-icon.weui-icon-warn:before {
-  font-size: 0.5rem !important;
-}
-
-.weui-icon-warn {
-  font-size: 0.5rem !important;
-}
 </style>
