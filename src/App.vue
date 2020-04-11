4bfdfbe92d3f734aaebf661077043e1633497376
Crack-Maker <2621161488@qq.com>
2020-04-11 19:10:03 +0800

    开屏gif

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,53 @@
 <template>
   <div id="app" style="height:100%;">
-	 <div>
-		 <router-view v-if="isRouterAlive"></router-view>
-<!-- 		 <TabBarDemo></TabBarDemo> -->
-	 </div>
+    <div>
+      <router-view v-if="isRouterAlive"></router-view>
+      <!-- 		 <TabBarDemo></TabBarDemo> -->
+    </div>
   </div>
 </template>
 
 <script>
-import TabBarDemo from './components/TabBarDemo.vue'
-import DrawerDemo from './components/DrawerDemo.vue'
+import TabBarDemo from "./components/TabBarDemo.vue";
+import DrawerDemo from "./components/DrawerDemo.vue";
 
 export default {
-  provide(){
-	  return{
-		  reload: this.reload
-	  }
+  provide() {
+    return {
+      reload: this.reload
+    };
   },
-  components: { 
-	TabBarDemo,
-	DrawerDemo
-	},
-  data () {
-      return {
-        isRouterAlive:true
-      }
-    },
-  methods:{
-	  reload(){
-		  this.isRouterAlive = false
-		  this.$nextTick(function(){
-			  this.isRouterAlive = true
-		  })
-	  }
+  components: {
+    TabBarDemo,
+    DrawerDemo
+  },
+  mounted(){
+    if (!localStorage.getItem("isOpened")) {
+      this.$router.push({
+        path: "/Opening"
+      });
+    }
+  },
+  data() {
+    return {
+      isRouterAlive: true
+    };
+  },
+
+  methods: {
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(function() {
+        this.isRouterAlive = true;
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -57,10 +65,10 @@ export default {
 
 <!-- 使用vux -->
 <style lang="less">
-@import '~vux/src/styles/reset.less'; 
+@import "~vux/src/styles/reset.less";
 </style>
 
-<style scoped>	
+<style scoped>
 </style>
 
 
---
+++ src/views/Opening.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="gif">
+    <img src="https://is-1254441798.cos.ap-shanghai.myqcloud.com/static/opening.gif" />
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    setTimeout(() => {
+      this.$router.push({
+        path: "/"
+      });
+    }, 6200);
+    localStorage.setItem("isOpened", true);
+  }
+};
+</script>
+<style scoped>
+img {
+  width: 11rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
